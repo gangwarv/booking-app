3eb61b5d978e828f8b464660032e74f723fab8db
vikas <vikash4654@gmail.com>
2020-01-27 18:23:32 +0530

    seatpicker assets added and enhanced

--- src/app/shared/seat-picker/seat-picker.component.ts
+++ src/app/shared/seat-picker/seat-picker.component.ts
@@ -6,25 +6,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./seat-picker.component.scss"]
 })
 export class SeatPickerComponent implements OnInit {
-  S = "https://img.icons8.com/android/24/000000/steering-wheel.png";
-  A = "https://img.icons8.com/material-outlined/24/000000/bus-seat-top-view.png";
-  B = "https://img.icons8.com/material-rounded/24/000000/bus-seat-top-view.png";
+  // S = "https://img.icons8.com/android/36/000000/steering-wheel.png";
+  // A = "https://img.icons8.com/material-outlined/36/000000/bus-seat-top-view.png";
+  // B = "https://img.icons8.com/material-rounded/36/000000/bus-seat-top-view.png";
+S = "/assets/layout/steering-wheel.png";
+  A = "/assets/layout/seat_booked.png";
+  B = "/assets/layout/sleeper_booked.png";
   // A = Avaliable
   // B = Booked
   // L = Locked
+  // layout = [
+  //   ["", "", "", "", "", "S"],
+  //   ["A", "A", "", "A", "A", "A"],
+  //   ["A", "B", "", "B", "B", "B"],
+  //   ["A", "A", "", "A", "A", "A"],
+  //   ["B", "A", "", "A", "A", "A"],
+  //   ["B", "B", "", "A", "A", "A"],
+  //   ["A", "A", "", "A", "A", "A"],
+  //   ["A", "A", "", "A", "A", "B"],
+  //   ["A", "A", "A", "A", "A", "A"]
+  // ];
   layout = [
-    ["", "", "", "", "", "S"],
-    ["A", "A", "", "A", "A", "A"],
-    ["A", "B", "", "B", "B", "B"],
-    ["A", "A", "", "A", "A", "A"],
-    ["B", "A", "", "A", "A", "A"],
-    ["B", "B", "", "A", "A", "A"],
-    ["A", "A", "", "A", "A", "A"],
-    ["A", "A", "", "A", "A", "B"],
-    ["A", "A", "A", "A", "A", "A"]
+    ["", "", "", "", "S"],
+    ["A", "A", "", "U", "U"],
+    ["A", "B", "", "B", "B"],
+    ["A", "A", "", "U", "U"],
+    ["B", "A", "", "A", "A"],
+    ["B", "B", "", "A", "A"],
+    ["A", "A", "", "A", "A"],
+    ["A", "A", "", "A", "B"],
+    ["A", "A", "A", "A", "A"]
   ];
+
 
   constructor() {}
 
   ngOnInit() {}
+
+  segmentChanged(event){
+
+  }
+
+  isSeat(i: number, j: number): boolean {
+    if(i>0){
+      return this.layout[i-1][j] !=='U';
+    }
+    return true;
+  }
 }
